12565ecc505cc22e6e248f8816bb782b60ca4a9c
Marquetta Matthews <mmatthews@chicagotechacademy.org>
2014-05-01 14:44:08 -0500

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fortyexamexam::Application.config.secret_key_base = '3239d4fe0e33266477e3df037b94df23b9a1e6e1edb5ea2a7dc228b548a18cdcf5e50194ea60bc1ab08e3553f1e6dc155ad55c830e2077bf47a44755748b70c8'
